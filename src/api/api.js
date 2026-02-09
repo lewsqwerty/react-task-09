525ab5371b6b3020006f602234a190d108e82fb9
lewsqwerty <rhmnisgod@gmail.com>
2026-02-09 16:51:42 +0400

    fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://json-server-roan-two.vercel.app",
   headers: { "Content-Type": "application/json" },
 });
 
